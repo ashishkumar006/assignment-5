3a1b7f2a65d4d5231f25a31a022f3482f5de95f6
Ashish <ashishkumarreddythodiam@gmail.com>
2024-12-18 20:57:03 +0530

    Add model tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,16 +1,13 @@
-import sys
-import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 import torch
+import pytest
 from model import CompactMNIST
 from train import train_one_epoch, count_parameters
 
 def test_parameter_count():
     model = CompactMNIST()
     param_count = count_parameters(model)
-    assert param_count < 25000, f"Model has {param_count} parameters, should be less than 25000"
+    assert param_count < 25000, f"Model has {param_count} parameters, which exceeds the limit of 25,000"
 
-def test_training_accuracy():
+def test_model_accuracy():
     accuracy = train_one_epoch()
-    assert accuracy > 95, f"Model accuracy is {accuracy}%, should be greater than 95%" 
+    assert accuracy > 95.0, f"Model accuracy {accuracy:.2f}% is below the required 95%"
