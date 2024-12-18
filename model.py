964ad89f2628e61cff7cef216feab7dd0af96ea7
Ashish <ashishkumarreddythodiam@gmail.com>
2024-12-18 21:32:17 +0530

    Add model tests

--- model.py
+++ model.py
@@ -5,37 +5,41 @@ import torch.nn.functional as F
 class CompactMNIST(nn.Module):
     def __init__(self):
         super(CompactMNIST, self).__init__()
-        # First convolution block
-        self.conv1 = nn.Conv2d(1, 12, 3, stride=1, padding=1)  # 28x28x12
+        # Initial convolution
+        self.conv1 = nn.Conv2d(1, 12, kernel_size=3, padding=1)
         self.bn1 = nn.BatchNorm2d(12)
         
-        # Second convolution block with residual connection
-        self.conv2a = nn.Conv2d(12, 16, 3, stride=2, padding=1)  # 14x14x16
-        self.bn2a = nn.BatchNorm2d(16)
-        self.conv2b = nn.Conv2d(16, 16, 3, padding=1)  # 14x14x16
-        self.bn2b = nn.BatchNorm2d(16)
+        # Residual block
+        self.conv2 = nn.Conv2d(12, 16, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(16)
+        self.conv3 = nn.Conv2d(16, 16, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(16)
         
-        # Third convolution block
-        self.conv3 = nn.Conv2d(16, 24, 3, stride=2, padding=1)  # 7x7x24
-        self.bn3 = nn.BatchNorm2d(24)
+        # Projection shortcut
+        self.shortcut = nn.Conv2d(12, 16, kernel_size=1)
         
-        self.dropout = nn.Dropout2d(0.2)
+        # Final layers
+        self.conv4 = nn.Conv2d(16, 24, kernel_size=3, padding=1)
+        self.bn4 = nn.BatchNorm2d(24)
         self.fc = nn.Linear(24 * 7 * 7, 10)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        # First block
+        # Initial convolution
         x = F.relu(self.bn1(self.conv1(x)))
+        identity = self.shortcut(x)
         
-        # Second block with residual
-        identity = self.conv2a(x)  # Projection shortcut
-        x = F.relu(self.bn2a(identity))
-        x = self.bn2b(self.conv2b(x))
+        # Residual block
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = self.bn3(self.conv3(x))
         x = F.relu(x + identity)  # Residual connection
         
-        # Third block
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.dropout(x)
+        # Final convolution
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.max_pool2d(x, 4)
         
+        # Fully connected
         x = x.view(-1, 24 * 7 * 7)
+        x = self.dropout(x)
         x = self.fc(x)
-        return x 
+        return x
